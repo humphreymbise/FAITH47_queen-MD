04abb0bc17a166540c856142911886175d9678c3
Humphrey47 <humphreymbise68@gmail.com>
2025-08-28 12:48:54 +0300

    Update privacy-tool.js

--- plugins/privacy-tool.js
+++ plugins/privacy-tool.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `` },
+                image: { url: `https://files.catbox.moe/q8hps6.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
