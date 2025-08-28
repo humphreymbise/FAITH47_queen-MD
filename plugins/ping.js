00c514e7fff6c6066c2b57958612443c8786d6a9
Humphrey47 <humphreymbise68@gmail.com>
2025-08-28 12:56:19 +0300

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -32,7 +32,7 @@ async (conn, mek, m, { from, quoted, sender, reply, }) => {
         const end = new Date().getTime();
         const responseTime = (end - start) / 1000;
 
-        const text = `> *CRISS-AI SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
+        const text = `> *queen_faith47 SPEED: ${responseTime.toFixed(2)}ms ${reactionEmoji}*`;
 
         await conn.sendMessage(from, {
             text,
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply, }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363417599637828@newsletter',
-                    newsletterName: "CRISS AI",
+                    newsletterJid: '',
+                    newsletterName: "queen_faith47-MD",
                     serverMessageId: 143
                 }
             }
@@ -69,7 +69,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const message = await conn.sendMessage(from, { text: '*PINGING...*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*🔥 CRISS-AI SPEED : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `*🌠queen_faith47 SPEED : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
