96054791b80fdb7d21e91a15aed0f7ba1691fe08
Humphrey47 <humphreymbise68@gmail.com>
2025-08-28 12:03:44 +0300

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -10,7 +10,7 @@ function replaceYouTubeID(url) {
 }
 
 cmd({
-    pattern: "play3",
+    pattern: "play1",
     alias: ["mp3", "ytmp3"],
     react: "🎵",
     desc: "Download Ytmp3",
@@ -44,7 +44,7 @@ cmd({
             `🔽 *Reply with your choice:*\n` +
             `1.1 *Audio Type* 🎵\n` +
             `1.2 *Document Type* 📁\n\n` +
-            `${config.FOOTER || "𓆩CRISS AI𓆪"}`;
+            `${config.FOOTER || "𓆩ZEZE TECH𓆪"}`;
 
         const sentMsg = await conn.sendMessage(from, { image: { url: image }, caption: info }, { quoted: mek });
         const messageID = sentMsg.key.id;
