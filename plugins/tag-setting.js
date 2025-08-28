cd1896ae9116d85a55ebff9b1dc240a01753527f
Humphrey47 <humphreymbise68@gmail.com>
2025-08-28 12:36:57 +0300

    Update tag-setting.js

--- plugins/tag-setting.js
+++ plugins/tag-setting.js
@@ -44,7 +44,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${mem.id.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ CRISS ┃ AI ✪──";
+        teks += "└──✪ FAITH ┃ 47✪──";
 
         conn.sendMessage(from, { text: teks, mentions: participants.map(a => a.id) }, { quoted: mek });
 
