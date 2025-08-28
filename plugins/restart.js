d81f1b0824116bb9e3890d0b593b1e8850e02d5a
Humphrey47 <humphreymbise68@gmail.com>
2025-08-28 12:42:13 +0300

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require("../lib/functions");
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart CRISS-AI",  
+    desc: "Restart faith47",  
     category: "owner",  
     filename: __filename  
 },  
