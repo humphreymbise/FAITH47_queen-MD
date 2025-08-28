de66fbe464497594a1cfd9fcb84850e6aa1baf16
Humphrey47 <humphreymbise68@gmail.com>
2025-08-28 12:26:34 +0300

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by CRISS AI 💜`
+      `> © Uploaded by zeze tech ❣️`
     );
 
   } catch (error) {
